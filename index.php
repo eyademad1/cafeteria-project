c8e061b1eceb1a8c8e7fd1ee4f968cc378a2e689
islam sobhy <islamsobhyeladly@gmal.com>
2026-03-15 13:50:21 +0200

    refactor: fix file paths for admin delete/toggle actions

--- index.php
+++ index.php
@@ -9,6 +9,21 @@ if ($page === 'logout') {
     exit;
 }
 
+
+$noOutputPages = ['admin_products_delete', 'admin_products_toggle', 'admin_users_delete'];
+if (in_array($page, $noOutputPages)) {
+    if (strpos($page, 'admin_products_') === 0) {
+        $subPage = str_replace('admin_products_', '', $page);
+        include __DIR__ . "/views/admin/products/{$subPage}.php";
+    } elseif (strpos($page, 'admin_users_') === 0) {
+        $subPage = str_replace('admin_users_', '', $page);
+        include __DIR__ . "/views/admin/users/{$subPage}.php";
+    }
+    exit;
+}
+
+include __DIR__ . "/views/layouts/header.php";
+
 switch ($page) {
     case 'login':
         include __DIR__ . "/views/auth/login.php";
@@ -21,8 +36,31 @@ switch ($page) {
         break;
     case 'admin':
         include __DIR__ . "/views/admin/dashboard.php";
-        break;    
+        break;
+    case 'admin_products':
+        include __DIR__ . "/views/admin/products/index.php";
+        break;
+    case 'admin_products_add':
+        include __DIR__ . "/views/admin/products/add.php";
+        break;
+    case 'admin_products_edit':
+        include __DIR__ . "/views/admin/products/edit.php";
+        break;
+    case 'admin_add_category':
+        include __DIR__ . "/views/admin/products/add_category.php";
+        break;
+    case 'admin_users':
+        include __DIR__ . "/views/admin/users/index.php";
+        break;
+    case 'admin_users_add':
+        include __DIR__ . "/views/admin/users/add.php";
+        break;
+    case 'admin_users_edit':
+        include __DIR__ . "/views/admin/users/edit.php";
+        break;
     default:
         include __DIR__ . "/views/auth/login.php";
 }
+
+include __DIR__ . "/views/layouts/footer.php";
 ?>
